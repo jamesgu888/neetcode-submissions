3d6d14170452699b6bce437a38e04b3197055099
jamesgu888 <139718358+jamesgu888@users.noreply.github.com>
2026-09-11 12:58:38 -0400

    Add: sort-colors - submission-8

---
+++ Data Structures & Algorithms/sort-colors/submission-8.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] counts = new int[3];
+
+        for (int num : nums) {
+            counts[num]++;
+        }
+
+        int count = 0;
+        for (int i = 0; i < counts.length; i++) {
+            for (int j = 0; j < counts[i]; j++) {
+                nums[count] = i;
+                count++;
+            }
+        }
+    }
+}
